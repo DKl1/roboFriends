07886820dea027b7935072e9cf7b30ec3f10733e
Данило Клим <danyloklym@Danilos-Laptop.local>
2024-02-23 16:15:17 +0200

    set up redux store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {Provider} from "react-redux";
+import {createStore, applyMiddleware, combineReducers} from "redux";
+import {thunk} from 'redux-thunk';
+import {createLogger} from "redux-logger/src";
 import 'tachyons';
 
 import './index.css';
 import App from "./containers/App"
+import {searchRobots, getRobotsReducer} from "./reducers";
+
+const logger = createLogger();
+
+const rootReducer = combineReducers({searchRobots, getRobotsReducer})
+const store = createStore(rootReducer, applyMiddleware(thunk, logger))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App/>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+
     </React.StrictMode>
 );
